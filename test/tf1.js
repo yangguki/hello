a96ec172bb543a9b5106aacc1007db142ea978b3
yangguki <46588664+yangguki@users.noreply.github.com>
2019-01-11 18:18:28 +0900

    Update tf1.js

--- test/tf1.js
+++ test/tf1.js
@@ -1,18 +1,1 @@
-console.log('tf1 file');
-
-console.log('tf1 파일');
-
-console.log('remote 에서 변경 파일');
-
-console.log('remote 에서 변경 파일2222');
-
-console.log('remote 에서 변경 파일2333');
-
-console.log('local 변경 1');
-console.log('local 변경 2');
-
-<<<<<<< HEAD
-console.log('remote  변경 발생 1');
-console.log('remote  변경 발생 2');
-=======
->>>>>>> refs/heads/local-hello-t1
+ts1 파일 완전 
